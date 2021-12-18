b085331d983f9fdebe51af555fed1b879ccaf04d
Rodrigo D’Agostino <rodrigo.dagostino@gmail.com>
2021-12-18 20:57:48 -0300

    feat: add search form, connect it to PokéAPI and display search result data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,66 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from 'vue'
+
+const searchValue = ref<string>( '' )
+const pokemonData = ref<PokemonDataInterface | null>( null )
+const idWithLeadingZeros = computed( () => {
+  if ( pokemonData.value ) {
+    return `00${ pokemonData.value.id }`.slice( -3 )
+  }
+  return null
+})
+
+interface TypeInterface {
+  type: {
+    name: string
+  }
+}
+
+interface PokemonDataInterface {
+  id: string
+  name: string
+  types: TypeInterface[]
+  sprites: {
+    front_default: string
+  }
+}
+
+const fetchPokemonData = async () => {
+  const apiUrl = 'https://pokeapi.co/api/v2'
+  try {
+    const response = await fetch( `${ apiUrl }/pokemon/${ searchValue.value }` )
+    const data = await response.json()
+    pokemonData.value = data
+  } catch ( error ) {
+    console.error( error )
+  }
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <h1>Vue Pokédex App</h1>
+  <form @submit.prevent="fetchPokemonData">
+    <input
+      type="text"
+      v-model="searchValue"
+      placeholder="Type in the name of your Pokémon…"
+    />
+    <button type="submit">Search</button>
+  </form>
+
+  <div v-if="pokemonData">
+    <img
+      :src="pokemonData.sprites.front_default"
+      width="96"
+      height="96"
+      :alt="pokemonData.name"
+    />
+    <p>#{{ idWithLeadingZeros }}</p>
+    <h2>{{ pokemonData.name }}</h2>
+    <ul v-for="{ type } in pokemonData.types" :key="type.name">
+      <li>{{ type.name }}</li>
+    </ul>
+  </div>
 </template>
 
 <style>
